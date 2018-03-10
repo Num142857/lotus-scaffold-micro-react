a3171825cf47487a21efa78f2cfbf543467612af
yangfan01 <yangfan01@jcgroup.com.cn>
2018-03-11 02:47:09 +0800

    微前端脚手架

---
+++ src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import singleSpaReact from 'single-spa-react';
+import './index.css';
+import App from './App';
+import registerServiceWorker from './registerServiceWorker';
+
+// ReactDOM.render(<App />, document.getElementById('root'));
+
+const reactLifecycles = singleSpaReact({
+    React,
+    ReactDOM,
+    rootComponent:() => <App />,
+    domElementGetter: () => document.getElementById('root')
+  });
+  
+  export const bootstrap = [
+    reactLifecycles.bootstrap,
+  ];
+  
+  export const mount = [
+    reactLifecycles.mount,
+  ];
+  
+  export const unmount = [
+    reactLifecycles.unmount,
+  ];
+
+  registerServiceWorker();
