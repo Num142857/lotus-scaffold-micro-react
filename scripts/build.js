cb379b9c7a980dbe23fd69dedd60f51e65620508
杨帆 <incomparable9527@foxmail.com>
2018-03-12 12:21:43 +0800

    fixed

--- scripts/build.js
+++ scripts/build.js
@@ -41,7 +41,6 @@ if (!checkRequiredFiles([paths.appHtml, paths.appIndexJs])) {
 
 // First, read the current file sizes in build directory.
 // This lets us display how much they changed later.
-
 measureFileSizesBeforeBuild(paths.appBuild)
   .then(previousFileSizes => {
     // Remove all content but keep the directory so that
