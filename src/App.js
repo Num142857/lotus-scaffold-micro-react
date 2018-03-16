2bb3b940e8511e5a7963d2c99db1eef28271757e
杨帆 <incomparable9527@foxmail.com>
2018-03-16 11:23:06 +0800

    fixed

--- src/App.js
+++ src/App.js
@@ -79,7 +79,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <TodoBox store={store} />
+        <TodoBox store={this.props.customProps.store.default} />
       </div>
     );
   }
