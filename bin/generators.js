efa64d096655f40cb4b7590e6f72dc0dc4147ad2
yangfan01 <yangfan01@jcgroup.com.cn>
2018-04-05 13:10:32 +0800

    加入commander

--- bin/generators.js
+++ bin/generators.js
@@ -1,54 +1,17 @@
 #!/usr/bin/env node
-var inquirer = require('inquirer')
-const util = require('lotusjs-util')
-const fse = require('fs-extra');
-const path = require('path');
-const shell = require("shelljs");
-const child_process = require('child_process')
-const lotusConfig = require('../.lotus/')
-const log = util.log
-inquirer
-  .prompt([{
-    type: 'input',
-    name: 'name',
-    message: '给你的项目取个名字吧',
-  }, ])
-  .then( async function(answers) {
 
-      for (let index = 0; index < lotusConfig.files.length; index++) {
-          const filePath = lotusConfig.files[index];
-          let basePath = path.resolve(__dirname, '../' + filePath)
-          let targetPath = process.cwd() + '/' + filePath
-          try {
-            await fse.copy(basePath, targetPath);
-            log.info("success:", filePath,'------>',targetPath)
-          } catch (error) {
-            log.warn("warn:", filePath,'------>',targetPath)
-            log.warn(error)
-          }
-      }
+const program = require('commander');
+const appInfo = require('../package.json')
 
-    let packageConfig
-    try {
-        packageConfig = await fse.readJson(`${process.cwd()}/package.json`)
-        packageConfig.name = answers.name
-        packageConfig.registerConfig.name = answers.name
-        packageConfig.registerConfig.prefix = `/${answers.name}/`
-        packageConfig.registerConfig.main = `/${answers.name}/main.js`
-        packageConfig.registerConfig.store = `/${answers.name}/store.js`
-    } catch (error) {
-        log.warn('package读取失败',error)
-    }
-
-    try {
-      await fse.writeJson(`${process.cwd()}/package.json`, packageConfig ,{spaces:2})
-      log.info("脚手架生成完毕")
-      process.exit(0);
-    } catch (error) {
-      process.exit(1);
-      log.warn('脚手架生成失败',error)
-    }
+program
+  .version(appInfo.version, '-v, --version')
+  .option('-i, --init', '初始化')
 
 
-    // log.info(answers,lotusConfig)
-  });
+program
+  .command('init')
+  .description('初始化你的项目')
+  .action(require('./init').init);
+
+
+program.parse(process.argv);
