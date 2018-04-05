5ceafea5b0d8a420d484899fac37ca5d44987a28
杨帆 <incomparable9527@foxmail.com>
2018-04-05 20:36:27 +0800

    test

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
     appState = observable(this.props.store)
   }
   render() {
-    let  store  = appState;
+    let { store }  = appState;
     console.log(store)
     return (
       <div className="App">
