bc3b723210ac5f11a83af8df6cc7ddebe61c3fd7
Fan <incomparable9527@foxmail.com>
2018-04-26 00:19:31 +0800

    test

--- src/Store.js
+++ src/Store.js
@@ -1,27 +1,31 @@
-import { createStore } from 'redux';
+import {
+  createStore
+} from 'redux';
 
 const initialState = {
-    namespace:"base",
-    count: 0,
-    refresh:0
+  namespace: "base",
+  count: 0,
+  refresh: 0
 };
 
 function reducer(state = initialState, action) {
-    switch (action.type) {
-        case 'INCREMENT':
-            return {
-                count: state.count + 1
-            };
-        case 'DECREMENT':
-            return {
-                count: state.count - 1
-            };
-        case 'REFRESH':
-        let refresh = state.refresh ++
-            return Object.assign(state,{refresh});
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return Object.assign(state, {
+        count: state.count + 1
+      });
+    case 'DECREMENT':
+      return Object.assign(state, {
+        count: state.count - 1
+      });
+    case 'REFRESH':
+      let refresh = state.refresh++
+        return Object.assign(state, {
+          refresh
+        });
+    default:
+      return state;
+  }
 }
 
 export const storeInstance = createStore(reducer);
