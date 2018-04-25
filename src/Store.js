18bc8fb5a375be5fd845dd20210da67d2f567397
Fan <incomparable9527@foxmail.com>
2018-04-26 01:05:11 +0800

    test

--- src/Store.js
+++ src/Store.js
@@ -11,6 +11,9 @@ const initialState = {
 function reducer(state = initialState, action) {
   switch (action.type) {
     case 'INCREMENT':
+    for (let index = 0; index < 999999; index++) {
+      
+    }
       return {...state,count:state.count+1}
     case 'DECREMENT':
     return {...state,count:state.count-1}
