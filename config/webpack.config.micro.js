3e79f29572f243f36c1d83e09bdbf8bb0aeab00a
杨帆 <incomparable9527@foxmail.com>
2018-04-26 10:45:22 +0800

    test

--- config/webpack.config.micro.js
+++ config/webpack.config.micro.js
@@ -250,7 +250,7 @@ module.exports = {
             // it's runtime that would otherwise processed through "file" loader.
             // Also exclude `html` and `json` extensions so they get processed
             // by webpacks internal loaders.
-            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/, /\.(css|less)$/,],
+            exclude: [/\.(js|jsx|mjs)$/, /\.html$/, /\.json$/],
             options: {
               name: 'static/media/[name].[hash:8].[ext]',
               publicPath: packageConfig.registerConfig.prefix,
